2d72ecac7e9c993e3d1f4030b5eaa3d4acaaceaa
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-04-22 13:47:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+anusO1#6969 and Hyperz#0001 worked on this bot
+*/
+
 const Discord = require('discord.js');
 const client = new Discord.Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']  
