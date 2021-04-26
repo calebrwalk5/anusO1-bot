1c4a87e26b122e3dc1605df39596aadc94f924ff
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-04-26 17:30:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const COMMANDS = ["`!anus` in front of commands", "`help`", "`ping`", "`avatar`"
 const random = ["http://www.stealthboats.com/", "https://bitwave.tv/", "https://templeos.org/", "https://codeelegant.github.io/", "https://puginarug.com/", "https://longdogechallenge.com/", "https://hooooooooo.com/", "http://162.191.232.173:81/live/index.html?Language=0", "http://111111111111111111111111111111111111111111111111111111111111.com/", "https://www.facinghistory.org/resource-library/gallery/pre-war-sighet-visual-essay", "http://endless.horse/", "https://thatsthefinger.com/", "http://beesbeesbees.com/", "https://en.wikipedia.org/wiki/Oy_vey", "https://www.recipetineats.com/wp-content/uploads/2014/05/Homemade-Heinz-Baked-Beans_0-SQ.jpg", "https://boards.4chan.org/b/catalog", "https://i.redd.it/ldxne6s0coi41.jpg", "https://blueprint-api-production.s3.amazonaws.com/uploads/story/thumbnail/95319/90832f52-855a-41b7-830d-f9297b64d213.jpg", "https://www.youtube.com/watch?v=qD1gdgeg8V4&feature=youtu.be", "https://miro.medium.com/max/3840/1*cbiq9DqLP_UEVqIOBb6MqQ.jpeg", "https://www.geeksforgeeks.org/playing-star-wars-in-command-prompt/"];
 const memes = ["https://media.tenor.co/videos/fc76ea52cd799e0add2126fd32bfdfc5/mp4", "https://i.kym-cdn.com/entries/icons/original/000/022/081/shit.jpg", "https://media.discordapp.net/attachments/762001220951998495/836191637587099689/unknown.png", "https://cdn.discordapp.com/attachments/712740930456649839/814281461293121576/hentai.mp4", "https://metro.co.uk/wp-content/uploads/2019/08/PRI_81815498.jpg?quality=90&strip=all&zoom=1&resize=480%2C252", "https://i.4cdn.org/b/1619208345070.png", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F826115210981236736%2FXPMIs2Qi.jpg&f=1&nofb=1", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2F0esqc3ocptg21.jpg&f=1&nofb=1", "https://makeawebsitehub.com/wp-content/uploads/2017/08/ouaismaisbon-1024x481.jpg", "https://inktank.fi/wp-content/uploads/2014/05/weirdwebsites.jpg", "https://media.discordapp.net/attachments/813781764592893963/813815048471445534/image0.jpg", "https://cdn.discordapp.com/attachments/777918623293374474/836222993407279184/6787c34-1.gif", "https://media.discordapp.net/attachments/776865442195046421/835119386653622302/image0.png?width=973&height=543", "https://media.discordapp.net/attachments/776865442195046421/834631238915850290/IMG_20210421_232504_593.jpg?width=651&height=543", "https://media.discordapp.net/attachments/776865442195046421/834267365969100830/IMG_20210420_183503_673.jpg?width=820&height=543", "https://media.discordapp.net/attachments/776865442195046421/833790805662498887/IMG_20210419_154540_381.jpg?width=473&height=543", "https://media.discordapp.net/attachments/776865442195046421/832748416076480562/Capture_2020-05-27-15-51-27.png?width=642&height=543", "https://media.discordapp.net/attachments/776865442195046421/832425980953755688/image0-23.png?width=708&height=543", "https://media.discordapp.net/attachments/815736858944077835/836201633113178142/Screenshot_20210426-072554.png?width=842&height=543", "https://media.discordapp.net/attachments/776865442195046421/832242018528788531/IMG_20210414_221325_782.jpg", "https://media.discordapp.net/attachments/776865442195046421/831724887025385522/IMG_20210413_225734_848.jpg", "https://media.discordapp.net/attachments/777918623293374474/835150714547863595/FB_IMG_1619011331967.jpg?width=527&height=543", "https://media.discordapp.net/attachments/777918623293374474/834800755097337906/IMG_20210422_103952.jpg", "https://cdn.discordapp.com/attachments/777918623293374474/834752903809597440/Screenshot_20210422-070125.png", "https://media.discordapp.net/attachments/777918623293374474/833509132345933864/image0.png?width=675&height=543", "https://media.tenor.co/videos/438945218d708b6fe6d2f542d299f7a5/mp4", "https://youtu.be/6KfAJ1CS4LI", "https://media.discordapp.net/attachments/777918623293374474/835150714547863595/FB_IMG_1619011331967.jpg?width=527&height=543", "https://media.discordapp.net/attachments/777918623293374474/835139631417786428/image0.png?width=584&height=543", "https://cdn.discordapp.com/attachments/777918623293374474/835896175608987669/video0.mp4", "https://media.discordapp.net/attachments/777918623293374474/833509020286320650/D1SGGAEbKyZ1BKzQh1cVHTEvQwIFRmRhzUtFyymlnR8.png?width=410&height=543", "https://media.discordapp.net/attachments/776865442195046421/835266189429178378/Snapchat-1168544652.jpg?width=792&height=543", "https://media.discordapp.net/attachments/787459728031612948/787459814523273276/new_joe_bidome.png?width=965&height=543", "https://media.discordapp.net/attachments/762001219308224522/835269725574922310/image0.png?width=316&height=543", "https://img.17qq.com/images/gmcdgnmpdcv.jpeg", "https://youtu.be/6KfAJ1CS4LI", "https://cdn.discordapp.com/attachments/722716211208650809/834176149482373180/How_To_Be_Funny.mp4", "https://cdn.discordapp.com/attachments/776865442195046421/835287628991561808/Screenshot_20210423-185400.png"];
 const JELLY = "328731272497201154";
+const ACTIVITIES = ["!anus help", "anusO1 bot", "!anus", "i am trapped in a server"];
 
 var messages = 0;
 var coinflip = 0;
@@ -22,8 +23,12 @@ var randomMeme = 0;
 // Initialize the bot
 
 client.on('ready', ready => {
-	client.user.setPresence({ activity: { name: "!anus", type: "LISTENING" }, status: "online" }); 
-	ready();
+	console.log('Bot built by anusO1#6969');
+	console.log("ready");
+	setInterval(() => {
+        const index = Math.floor(Math.random() * (ACTIVITIES.length - 1) + 1);
+        client.user.setActivity(ACTIVITIES[index], { type: 'LISTENING' });
+    }, 10000);
 });
 
 // New user
@@ -123,11 +128,6 @@ client.on('message', message => { // i know, i know. i'm a bad programmer for do
 });
 
 // Functions
-
-function ready() {
-	console.log('Bot built by anusO1#6969');
-	console.log("ready");
-}
 
 function flipcoin() {
 	coinflip = Math.random() * (100 - 0);
